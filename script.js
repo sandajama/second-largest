e432af284c2ca9a41a2bd4e96b01ee9db42eb7f4
sandajama <sandajama29@gmail.com>
2023-06-21 15:03:16 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Good Luck 💪🏾
+function secondLargest(array){
+    let firstlarnum=(secondLarnum= Math.min(...array))
+    for(let i=0; i<array.length; i++){
+        if(array[i]>firstlarnum){
+            secondLarnum=firstlarnum
+            firstlarnum=array[i]
+        }
+        else{
+            if(array[i]>secondLarnum && array[i] !==firstlarnum){
+                secondLarnum=array[i]
+            }
+        }
+    }
+   
+return secondLarnum
+
+}
+console.log (secondLargest([23, 66, 50, 12, 6]))
+console.log (secondLargest([65, 34, 100, 22, 85]))
+console.log (secondLargest([67, 22, 19, 12, 80]) )
